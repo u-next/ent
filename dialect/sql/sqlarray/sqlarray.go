aa87f3ae745abd41ab5cb184d0bf2aa6983d369f
a-zhu-unext <a-zhu@unext.jp>
2025-09-30 23:43:25 +0900

    feat: implement UnnestBuilder for UNNEST expression support in GoogleSQL

--- dialect/sql/sqlarray/sqlarray.go
+++ dialect/sql/sqlarray/sqlarray.go
@@ -11,3 +11,53 @@ func Array(e sql.Querier) sql.Querier {
 		})
 	})
 }
+
+// UnnestBuilder is a builder for the UNNEST expression.
+type UnnestBuilder struct {
+	sql.TableView
+	expr   sql.Querier
+	alias  string
+	offset string
+}
+
+// Unnest wraps the array expression with the UNNEST operator (GoogleSQL).
+func Unnest(array sql.Querier) *UnnestBuilder {
+	return &UnnestBuilder{
+		expr: array,
+	}
+}
+
+func (u *UnnestBuilder) As(alias string) *UnnestBuilder {
+	u.alias = alias
+	return u
+}
+
+func (u *UnnestBuilder) WithOffset() *UnnestBuilder {
+	u.offset = "offset"
+	return u
+}
+
+func (u *UnnestBuilder) WithOffsetAs(offset string) *UnnestBuilder {
+	u.offset = offset
+	return u
+}
+
+func (u *UnnestBuilder) Query() string {
+	b := &sql.Builder{}
+	b.WriteString("UNNEST ")
+	b.Wrap(func(b *sql.Builder) {
+		b.Join(u.expr)
+	})
+	if u.alias != "" {
+		b.WriteString(" AS ")
+		b.WriteString(u.alias)
+	}
+	if u.offset != "" {
+		b.WriteString(" WITH OFFSET")
+		if u.offset != "offset" {
+			b.WriteString(" AS ")
+			b.WriteString(u.offset)
+		}
+	}
+	return b.String()
+}
